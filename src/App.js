2759927ad6735b7f8c97c397da7c786a831a9a3c
RichGod <usenrichgod93@gmail.com>
2023-03-05 11:41:06 +0100

    demo: pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import { Login, Signup } from "./modules/learner/pages";
 
 function App() {
   return (
-    <div className="App flex justify-center">
-      <p className=' text-6xl font-mono'>Hello Snr Dev RichGod</p>
+    <div className="App flex flex-1 justify-center">
+      <Signup />
+      {/* <Login /> */}
     </div>
   );
 }
