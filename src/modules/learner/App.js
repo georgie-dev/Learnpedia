d0859defbd5a24cb41b5abe94116c4ff77c0f281
RichGod <usenrichgod93@gmail.com>
2023-03-10 00:20:13 +0100

    no change from pull

--- src/modules/learner/App.js
+++ src/modules/learner/App.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import { Header, Sidebar } from './components'
-import { Outlet } from 'react-router-dom'
+import { Header, Sidebar } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div >
-    <div className="flex relative dark:bg-main-dark-bg">
-    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white z-50">
-        <Sidebar/>
-    </div>
-    <div  
-    className='min-h-screen md:ml-64 w-full  '
-      >
-        <div className='relative  md:static  dark:bg-main-dark-bg navbar w-full'>
-            <Header/>
+    <div>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white z-50">
+          <Sidebar />
         </div>
-        <div >
-          <Outlet/>
-        </div>
-        {/* <div className='p-12 dark:bg-main-dark-bg'>
+        <div className="min-h-screen md:ml-64 w-full">
+          <div className="relative  md:static  dark:bg-main-dark-bg navbar w-full">
+            <Header />
+          </div>
+          <div>
+            <Outlet />
+          </div>
+          {/* <div className='p-12 dark:bg-main-dark-bg'>
       <Footer/>
     </div> */}
+        </div>
+      </div>
     </div>
-    </div>
-</div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
